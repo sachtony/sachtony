773372a52a20bad2faafcf9bc43abca86f835d5b
Sachtony <24889366+sachtony@users.noreply.github.com>
2022-12-10 21:57:24 +0700

    Create oauth.py

---
+++ oauth.py
@@ -0,0 +1,7 @@
+from stytch import Client
+
+client = Client(
+    project_id="project-test-26bff1c0-20ff-4656-b9ae-e3b1f0b96d7d",
+    secret="secret-test-61AFPBDhb7PHZhCUqz_A1ynzfhfw9BdOTsg=",
+    environment="test",
+)
